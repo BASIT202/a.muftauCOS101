e2696fef11ce5c41614e2eabb30330a102253876
a.muftauCOS101 <muftaubasit7@gmail.com>
2023-10-22 15:43:51 +0100

    (my first commit)

---
+++ week-1/practice-1.rs
@@ -0,0 +1,42 @@
+fn fn main() {
+	println!("Welcome to COS 101");
+
+	println!("The course learningoutcome is as follows:");
+
+	println!("1. Distinguish between computer and computer programming,/n 
+		      2. Develop some techinques in computer science,/n
+		      3. Understand the different areas of study in computer science,/n 
+		      4. Conversant with applications of computer science,/n 
+		      5. Navigate the carrer prospects in computer science./n 
+		      6. Conversant with computer programming concepts.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
